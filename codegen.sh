e35d541d933f032d893cdf30a21f0681295414b2
tomasen <tomasen@gmail.com>
2018-07-21 16:25:33 +0800

    add ts defination

--- codegen.sh
+++ codegen.sh
@@ -9,6 +9,6 @@ protoc -I . --go_out=plugins=grpc:src/go/ training/*/training.proto
 
 # nodejs, static codegen
 
-grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --ts_out="src/nodejs/" --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` translation/*/*.proto training/*/*.proto media/*/*.proto health/*/*.proto
-grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --ts_out="src/nodejs/" --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` google/*/*.proto
-grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --ts_out="src/nodejs/" --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` google/cloud/speech/*/*.proto
+grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --ts_out=src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` translation/*/*.proto training/*/*.proto media/*/*.proto health/*/*.proto
+grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` google/*/*.proto
+grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` google/cloud/speech/*/*.proto
