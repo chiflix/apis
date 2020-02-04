707b1347f9cd81e478efe26ee5dfe98c9d43297e
Max <ymmuse@gmail.com>
2020-02-04 13:17:14 +0900

    fix codegen.sh

--- codegen.sh
+++ codegen.sh
@@ -2,17 +2,17 @@
 
 # golang
 
-# protoc -I . --go_out=plugins=grpc:src/go/ media/*/*.proto
-# protoc -I . --go_out=plugins=grpc:src/go/ health/*/*.proto
-# protoc -I . --go_out=plugins=grpc:src/go/ translation/*/translation.proto
-# protoc -I . --go_out=plugins=grpc:src/go/ training/*/training.proto
+protoc -I . --go_out=plugins=grpc:src/go/ media/*/*.proto
+protoc -I . --go_out=plugins=grpc:src/go/ health/*/*.proto
+protoc -I . --go_out=plugins=grpc:src/go/ translation/*/translation.proto
+protoc -I . --go_out=plugins=grpc:src/go/ training/*/training.proto
 
-# # nodejs, static codegen
+# nodejs, static codegen
 
-# grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --ts_out=src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` translation/*/*.proto training/*/*.proto media/*/*.proto health/*/*.proto
-# protoc --plugin=protoc-gen-ts=`which protoc-gen-ts` --ts_out=./src/nodejs translation/*/*.proto training/*/*.proto media/*/*.proto health/*/*.proto
-# grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` google/*/*.proto
-# grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` google/cloud/speech/*/*.proto
+grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --ts_out=src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` translation/*/*.proto training/*/*.proto media/*/*.proto health/*/*.proto
+protoc --plugin=protoc-gen-ts=`which protoc-gen-ts` --ts_out=./src/nodejs translation/*/*.proto training/*/*.proto media/*/*.proto health/*/*.proto
+grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` google/*/*.proto
+grpc_tools_node_protoc -I . --js_out=import_style=commonjs,binary:src/nodejs/ --grpc_out=src/nodejs --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` google/cloud/speech/*/*.proto
 
 # swift
 
