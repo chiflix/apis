fdc3b5c937cece406013765c8f92f452ab2e7cfe
Max <ymmuse@gmail.com>
2020-02-19 01:34:22 +0900

    build iphoneos iphonesimulator

--- src/c++/ios_build.sh
+++ src/c++/ios_build.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
+set -e
 
-mkdir -p ios
+BUILD="Release"
+BUILD_ROOT=`pwd`
+
+if [ ! -d ./ios ]; then
+    mkdir -p ios
+fi
+
 cd ios
 
-Targets=(   "iphonesimulator:x86_64"
-            "iphoneos:arm64" )
+Targets=(   "iphoneos:OS64"
+            "iphonesimulator:SIMULATOR64" )
+
 
 for target in "${Targets[@]}" ; do
     SDK="${target%%:*}"
     ARCH="${target##*:}"
     
-    mkdir $SDK && cd $SDK
-    
-    cmake ../../ -GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_SYSROOT=$SDK -DCMAKE_OSX_ARCHITECTURES=$ARCH -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -DgRPC_BUILD_CODEGEN=OFF -DCARES_INSTALL=OFF -DCARES_BUILD_TOOLS=OFF -DENABLE_BITCODE=TRUE -DENABLE_ARC=TRUE -Dprotobuf_BUILD_TESTS=OFF
+    if [ -d ./$SDK ]; then 
+        continue
+    fi 
 
-    xcodebuild -configuration "Release" -target SagiAPI GCC_WARN_64_TO_32_BIT_CONVERSION=no
+    if [ ! -d ./ios ]; then
+        mkdir $SDK
+    fi
+    cd $SDK
+    
+    # cmake ../../ -GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_SYSROOT=$SDK -DCMAKE_OSX_ARCHITECTURES=$ARCH -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -DgRPC_BUILD_CODEGEN=OFF -DCARES_INSTALL=OFF -DCARES_BUILD_TOOLS=OFF -DENABLE_BITCODE=TRUE -DENABLE_ARC=TRUE -Dprotobuf_BUILD_TESTS=OFF
+
+    cmake ../../ -GXcode -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_SYSROOT=$SDK -DPLATFORM=$ARCH -DENABLE_BITCODE=1 -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -DgRPC_BUILD_CODEGEN=OFF -DCARES_INSTALL=OFF -DCARES_BUILD_TOOLS=OFF -DENABLE_BITCODE=TRUE -DENABLE_ARC=TRUE -Dprotobuf_BUILD_TESTS=OFF
+
+    xcodebuild -project SagiAPI.xcodeproj -configuration $BUILD -target SagiAPI GCC_WARN_64_TO_32_BIT_CONVERSION=no
 
     cd -
 done
+
+if [ ! -d ./bin ]; then
+    mkdir -p bin/include
+    mkdir -p bin/lib
+fi
+
+LIPOFiles=""
+for target in "${Targets[@]}" ; do
+    SDK="${target%%:*}"
+    LIPOFiles="${LIPOFiles} $BUILD_ROOT/ios/$SDK/${BUILD}-${SDK}/libSagiAPI.a"
+done
+
+xcrun lipo -create $LIPOFiles -output $BUILD_ROOT/ios/bin/lib/libSagiAPI.a
+xcrun lipo -info $BUILD_ROOT/ios/bin/lib/libSagiAPI.a
