13c73159b340de175417b82065a7a3ec67d7b494
Max <ymmuse@gmail.com>
2020-02-19 15:05:14 +0900

    adjust build script

--- src/c++/ios_build.sh
+++ src/c++/ios_build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-BUILD="Release"
-BUILD_ROOT=`pwd`
+#  ./ios_build.sh Release (or Debug)
+
+BUILD_ROOT=="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+BUILD=$1
 
 if [ ! -d ./ios ]; then
     mkdir -p ios
@@ -25,6 +28,7 @@ for target in "${Targets[@]}" ; do
     if [ ! -d ./ios ]; then
         mkdir $SDK
     fi
+
     cd $SDK
     
     # cmake ../../ -GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_SYSROOT=$SDK -DCMAKE_OSX_ARCHITECTURES=$ARCH -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -DgRPC_BUILD_CODEGEN=OFF -DCARES_INSTALL=OFF -DCARES_BUILD_TOOLS=OFF -DENABLE_BITCODE=TRUE -DENABLE_ARC=TRUE -Dprotobuf_BUILD_TESTS=OFF
@@ -38,14 +42,29 @@ done
 
 if [ ! -d ./bin ]; then
     mkdir -p bin/include
-    mkdir -p bin/lib
+    mkdir -p bin/lib/iphoneos
+    mkdir -p bin/lib/iphonesimulator
+    mkdir -p bin/lib/combined
 fi
 
 LIPOFiles=""
 for target in "${Targets[@]}" ; do
     SDK="${target%%:*}"
-    LIPOFiles="${LIPOFiles} $BUILD_ROOT/ios/$SDK/${BUILD}-${SDK}/libSagiAPI.a"
+    afile="$BUILD_ROOT/ios/$SDK/${BUILD}-${SDK}/libSagiAPI.a"
+    
+    if [ ! -f $afile ]; then
+        echo "$afile not exist" && exit -1
+    fi
+
+    LIPOFiles="${LIPOFiles} $afile"
+    cp -f $afile ./bin/lib/$SDK
+    xcrun lipo -info $afile
 done
 
-xcrun lipo -create $LIPOFiles -output $BUILD_ROOT/ios/bin/lib/libSagiAPI.a
-xcrun lipo -info $BUILD_ROOT/ios/bin/lib/libSagiAPI.a
+xcrun lipo -create $LIPOFiles -output "$BUILD_ROOT/ios/bin/lib/combined/libSagiAPI.a"
+xcrun lipo -info "$BUILD_ROOT/ios/bin/lib/combined/libSagiAPI.a"
+
+DirList="translation training media health"
+for dir in $DirList ; do
+    cp -rf "$BUILD_ROOT/src/$dir/v1/*.h" "$BUILD_ROOT/ios/bin/include"
+done
